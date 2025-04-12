fcedd763626c1d04b758f97ac880103cce089d0c
akif999 <aki01nf28@gmail.com>
2025-04-12 17:01:31 +0900

    update files of chapter2

--- 2_perceptron/main.py
+++ 2_perceptron/main.py
@@ -4,25 +4,61 @@
 # (x1, x2) = (1, 0) , 0.5 + 0.0 > 0.3
 # (x1, x2) = (0, 1) , 0.0 + 0.5 > 0.3
 # (x1, x2) = (1, 1) , 0.5 + 0.5 > 0.3
+import numpy as np
+
 
 # AND
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w2 + x2*w2
-    if tmp <= theta:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
-        return 1
     else:
-        # never execute
-        pass
+        return 1
+
+
+# NAND
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+# OR
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
 
 
 def main():
+    print('AND')
     print(AND(0, 0))
     print(AND(1, 0))
     print(AND(0, 1))
     print(AND(1, 1))
+    print('NAND')
+    print(NAND(0, 0))
+    print(NAND(1, 0))
+    print(NAND(0, 1))
+    print(NAND(1, 1))
+    print('OR')
+    print(OR(0, 0))
+    print(OR(1, 0))
+    print(OR(0, 1))
+    print(OR(1, 1))
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ゼロから作る Deep Learning
 
+## References
 
+* https://ja.wikipedia.org/wiki/%E6%95%B0%E5%AD%A6%E8%A8%98%E5%8F%B7%E3%81%AE%E8%A1%A8
